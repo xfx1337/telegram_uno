8d6baa0c7fa6950f301a33b700819a0469bca2fd
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-20 23:35:43 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ def send_text(message):
                         string = str(i) + " " + str(Player1_deck[i])
                         bot.send_message(Lobby[-2], string)
                     num_sent = bot.send_message(Lobby[-2], "Выбери карту(цифруили если нет карты напиши take):")
-                    bot.register_next_step_handler(num_sent, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
+                    bot.register_next_step_handler(num_sent, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed, message))
                 if Lobby_now == Lobby[-1]:
                     for i in range(len(Player2_deck)):
                         string = str(i) + " " + str(Player2_deck[i])
