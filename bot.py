ac426b6790a72c7cd819a0c47b876cbae76f654e
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-21 18:54:51 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -434,7 +434,7 @@ def send_text(message):
                     Player_now_UCard_Number = choosed[-2]
                     classic(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed)
             def ask2_2(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed):
-                while (message.text.lower() == "lobby") or (message.text == "Lobby"):
+                while (message.text.lower() == "lobby") or (message.text == "Lobby") or (message is None) or (message.text is None):
                     return
                 if message.text.lower == "take":
                     Player2_deck.append(choose_card())
