943c19345c8e79da3df0d67219a13d3e8c2bf8d1
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-21 14:45:21 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -83,9 +83,8 @@ def send_text(message):
         minPlayers = int("2")
         while int(count) < minPlayers:
             print("not enough")
-        while int(count) >= minPlayers:
-            print("enough")
-        print(Lobby)
+            count = len(Lobby)
+        print("enough")
 bot.polling()
 
 
