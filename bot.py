b4da64e3482be1ddecbc504b47ca6f60ed0e9b1d
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-23 17:56:13 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -207,7 +207,7 @@ def send_text(message):
                 mes = message.text
                 if not mes.isdigit() or message.text.lower() != "take" or message.text is None:
                     msg = bot.send_message(Lobby[-2], "Попробуй снова!:")
-                    bot.register_next_step_handler(msg, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
+                    bot.register_next_step_handler(msg, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed, message))
                     return
                 if message.text.lower == "take":
                     Player1_deck.append(choose_card())
