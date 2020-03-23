82dab3dea7a3d3b44a6779497fad32dcd6c7c53b
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-23 16:56:09 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -205,6 +205,7 @@ def send_text(message):
   
             def ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed):
                 if message.text == "Lobby":
+                    x_SENT = bot.send_message(Lobby[1], "")
                     bot.register_next_step_handler(x_SENT, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
                 try:
                     int(message.text)
