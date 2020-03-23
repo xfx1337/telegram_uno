db82a384a5d8806d3e6f36663d9d4720c1f40123
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-23 16:51:15 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -209,7 +209,7 @@ def send_text(message):
                 try:
                     int(message.text)
                     pass
-                except: TypeError:
+                except TypeError:
                     bot.register_next_step_handler(x_SENT, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
                 except ValueError:
                     pass
