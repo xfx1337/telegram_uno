5b40cf1145104b1f3290184c3dc2670348052bb9
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-03-23 17:03:43 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -204,19 +204,8 @@ def send_text(message):
                     ask(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed)
   
             def ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed):
-                if message.text == "Lobby":
-                    x_SENT = bot.send_message("891078895", "Just")
-                    bot.register_next_step_handler(x_SENT, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
-                try:
-                    int(message.text)
-                    pass
-                except TypeError:
-                    x_SENT = bot.send_message("891078895", "Just")
-                    bot.register_next_step_handler(x_SENT, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player1_cards, Player2_cards, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player_now_cards, PNC, Player1_deck, Player2_deck, nw, choosed))
-                except ValueError:
-                    pass
-
-
+                update.message.text
+                print(update.message.text)
 
 
                 
