39ae0fa9f0e46347b90489cae85264b8b355b873
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-04-10 03:05:06 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -244,16 +244,14 @@ def send_text(message):
                         string = str(card) + " " + str(Player1_deck[card])
                         bot.send_message(Lobby[-2], string)
                     num_sent = bot.send_message(Lobby[-2], "Выбери карту(цифра или пропиши take что взять карту из колоды):")
-                    choosed = []
-                    PNC == int("7")
-                    bot.register_next_step_handler(num_sent, ask2_1(Player_now_UCard_Color, Player_now_UCard_Number, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player1_deck, Player2_deck, choosed, PNC))
+                    bot.register_next_step_handler(num_sent, ask2_1(message, Player_now_UCard_Color, Player_now_UCard_Number, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player1_deck, Player2_deck, choosed, PNC))
                 if Lobby_now == Lobby[-1]:
                     for card in range(len(Player2_deck)):
                         string = str(card) + " " + str(Player2_deck[i])
                         bot.send_message(Lobby[-1], string)
                     num_sent = bot.send_message(Lobby[-1], "Выбери карту(цифра или ake что бы взять карту из колоды):")
                     choosed = []
-                    bot.register_next_step_handler(num_sent, ask2_2(Player_now_UCard_Color, Player_now_UCard_Number, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player1_deck, Player2_deck, choosed, PNC))
+                    bot.register_next_step_handler(num_sent, ask2_2(message, Player_now_UCard_Color, Player_now_UCard_Number, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player1_deck, Player2_deck, choosed, PNC))
             ask(message, Player_now_UCard_Color, Player_now_UCard_Number, Player_last_UCard_Number, Player_last_UCard_Color, Lobby_now, Player1_deck, Player2_deck, choosed, PNC)
     def uno_start():
             message = ""
