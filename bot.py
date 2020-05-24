d6ffbfc64ef13b0a78e2b30b39f962ac65733a8c
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-05-25 00:30:11 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,14 +5,18 @@ import SimpleQIWI
 from SimpleQIWI import *
 from time import sleep
 from telebot import types
+
 bot = telebot.TeleBot('964957577:AAHQlnTDLdyLxDsrnsSE8M0HcxRwMup6YDk')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, False)
+
 keyboard1.row('Поддержка', 'Меню')
 keyboard1.row('Баланс', 'Пополнить', 'Новости')
 keyboard1.row('Создать карту')
 keyboard1.row('Снять с карты')
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    msg = "Иди нахуй!"
     bot.send_message(message.chat.id, msg, reply_markup=keyboard1)
 
 @bot.message_handler(content_types=['text'])
@@ -21,30 +25,57 @@ def send_text(message):
         conn = sqlite3.connect("cards.db") # или :memory: чтобы сохранить в RAM
         cursor = conn.cursor()
         user_id = message.chat.id
-        zero_str = ("0")
-        holder = message.chat.first_name + " | " + message.chat.last_name
-        holder = str(holder)
-        zero = int(zero_str)
-        test2 = [((user_id), (zero), (holder))]
-        cursor.executemany("INSERT INTO list VALUES (?,?,?)", test2)
-        conn.commit()
+        holder = str(message.chat.first_name + " | " + message.chat.last_name)
+        test2 = [(user_id, 0, holder)]
+
+        cardCheck = ("SELECT * FROM list WHERE ID LIKE '") + (str(user_id)) + ("%'")
+        cursor.execute(cardCheck)
+
+        idCheck = cursor.fetchone()
+
+        if idCheck == None:
+            hasCard = 0
+        else:
+            idCheck = [ idCheck[-3] ]
+            idCheck = str(idCheck)
+            idCheck = idCheck.replace("'", "")
+            idCheck = idCheck.replace("[", "")
+            idCheck = idCheck.replace("]", "")
+            hasCard = 1
+        
+        if hasCard == 1 and int(idCheck) == int(user_id):
+            bot.send_message(message.chat.id, "У вас уже есть карта!")
+            
+        elif hasCard == 0:
+            cursor.executemany("INSERT INTO list VALUES (?,?,?)", test2)
+            bot.send_message(message.chat.id, "Карта успешно созданна!")
+            conn.commit()
+            
         conn.close()
+        
     elif message.text.lower() == 'баланс':
         conn = sqlite3.connect("cards.db") # или :memory: чтобы сохранить в RAM
         cursor = conn.cursor()
         user_id = message.chat.id
+        
         bot.send_message(message.chat.id, "Ваш ID:%i" % (user_id))
-        user_id2 = str(user_id)
-        sqlBalance = ("SELECT * FROM list WHERE ID LIKE '") + (user_id2) + ("%'")
+        
+        sqlBalance = ("SELECT * FROM list WHERE ID LIKE '") + (str(user_id)) + ("%'")
         cursor.execute(sqlBalance)
+        
         balance = cursor.fetchone()
-       	balance2 = [ balance[-1] ]
-       	balance3 = str(balance2)
-       	balance4 = balance3.replace("'", "")
-       	balance5 = balance4.replace("[", "")
-       	balance5 = balance5.replace("]", "")
-        balance_send = ("Баланс: ") + (balance5)
-        bot.send_message(message.chat.id, balance_send) 
+        balance = [ balance[-2] ]
+        balance = str(balance)
+        balance = balance.replace("'", "")
+        balance = balance.replace("[", "")
+        balance = balance.replace("]", "")
+        balance = ("Баланс: ") + (balance)
+        
+        bot.send_message(message.chat.id, balance)
+
+    elif message.text.lower() == 'новости':
+        bot.send_message(message.chat.id, "Хуй тебе, а не новости!")
+        
     elif message.text.lower() == 'пополнить':
         #conn = sqlite3.connect("transfers.db") # или :memory: чтобы сохранить в RAM
         #cursor = conn.cursor()
@@ -86,14 +117,13 @@ def send_text(message):
                     sql0 = "SELECT * FROM list WHERE ID LIKE '" + (user_id2) + ("'")
                     cursor.execute(sql0)
                     balance_old = (cursor.fetchone())
-                    balance_old2 = [ balance_old[-1] ]
-                    balance_old3 = str(balance_old2)
-                    balance_old4 = balance_old3.replace("'", "")
-                    balance_old5 = balance_old4.replace("[", "")
-                    balance_old6 = balance_old5.replace("]", "")
-                    balance_old7 = int(balance_old6)
+                    balance_old = [ balance_old[-1] ]
+                    balance_old = balance_old.replace("'", "")
+                    balance_old = balance_old.replace("[", "")
+                    balance_old = balance_old.replace("]", "")
+                    balance_old = int(balance_old)
                     price3 = int(user_price_input)
-                    balance = (balance_old7) + (price3)
+                    balance = (balance_old) + (price3)
                     sql = ('''
 UPDATE list
 SET BALANCE = '%i'
@@ -103,12 +133,57 @@ WHERE ID = '%s'
                     conn.commit()
                     conn.close()
                     break
+                
         sent = bot.send_message(message.chat.id, "Введите сумму для перевода на ваш баланс!")
 
         bot.register_next_step_handler(sent, transfer)
         sleep(1)
+    
+    elif message.text.lower() == "снять с карты":
+        def transferback():
+            if message.text.isnumeric() == False:
+                bot.send_message(message.chat.id, "Неверный тип ввода! Попробуйте снова!")
+                status = 0
+            elif message.text.isnumeric():
+                status = 1
+            
+            
+            conn = sqlite3.connect("cards.db") # или :memory: чтобы сохранить в RAM
+            cursor = conn.cursor()
+            user_id = message.chat.id
+        
+        
+            sqlBalance = ("SELECT * FROM list WHERE ID LIKE '") + (str(user_id)) + ("%'")
+            cursor.execute(sqlBalance)
+        
+            balance = cursor.fetchone()
+            balance = [ balance[-2] ]
+            balance = str(balance)
+            balance = balance.replace("'", "")
+            balance = balance.replace("[", "")
+            balance = balance.replace("]", "")
+            global ammounttotransfer = message.text
+            if status = 1:
+                if (message.text) > balance:
+                    bot.send_message(message.chat.id, "Недостаточно средств на балансе!!!")
+                elif balance > message.text:
+                    
+                    def transfernext:
+                        number = str("+7") + str(message.text)
+                        global ammounttotransfer
+                        api.pay(account=number, amount=ammounttotransfer, comment='Платёж от бота!')
+                        bot.send_message(message.chat.id, "Платёж переведён!")
+                    
+                    
+                    
+                    
+                    sendnumber = bot.send_message(message.chat.id, "Введите номер телефона +7:")
+                    bot.register_next_step_handler(sendnumber, transfernext)
+        sendask = bot.send_message(message.chat.id, "Сколько ты хочешь снять с карты?")
+        bot.register_next_step_handler(sendask, transferback)
+        
     else:
-        bot.send_message(message.chat.id, 'Упс! Ты ввёл не правельную комманду! Пиши "Меню" что получить подсказку!')
+        bot.send_message(message.chat.id, 'Упс! Ты тупой гандон! Нажни нормально!')
 
 #keyboard = types.InlineKeyboardMarkup();
 bot.polling()
